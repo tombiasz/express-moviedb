aac92ea7a819a6ba95a7d2d08e8f7f984d5d7e90
tombiasz <tomasz.m.nowacki@gmail.com>
2018-04-22 18:20:21 +0200

    use es6 features

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
+const createError = require('http-errors');
+const express = require('express');
 const dotenv = require('dotenv');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const Sequelize = require('sequelize');
 
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+const app = express();
 
 dotenv.config();
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,12 +28,10 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use((req, res, next) => next(createError(404)));
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -45,11 +44,7 @@ app.use(function(err, req, res, next) {
 const sequelize = new Sequelize(process.env.DATABASE_URL, { native: true });
 sequelize
   .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch(err => console.error('Unable to connect to the database:', err));
 
 module.exports = app;
