5e9d4a12aac1ce383de2fd19c45827dd25e02d31
tombiasz <tomasz.m.nowacki@gmail.com>
2018-04-23 18:48:00 +0200

    disable quoteIdentifiers

--- config/config.js
+++ config/config.js
@@ -7,6 +7,7 @@ module.exports = {
   development: {
     use_env_variable: 'DATABASE_URL',
     native: true,
+    quoteIdentifiers: false,
   },
   production: {
     use_env_variable: 'DATABASE_URL',
