b419dd76cb2a90bb21e9e70855d7b7fd25dee966
tombiasz <tomasz.m.nowacki@gmail.com>
2018-04-24 09:09:43 +0200

    remove static files

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/movies', moviesRouter);
 app.use('/comments', commentsRouter);
