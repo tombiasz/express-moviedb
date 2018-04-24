1c6489b733d3ce8694dfcb16b6c06de3268f3323
tombiasz <tomasz.m.nowacki@gmail.com>
2018-04-24 16:37:22 +0200

    test comment attributes and associations

--- test/models/comment.test.js
+++ test/models/comment.test.js
@@ -3,6 +3,7 @@ const mocha = require('mocha');
 
 const dbUtils = require('../utils/db');
 const movieFactory = require('../factories/movie');
+const commentFactory = require('../factories/comment');
 
 
 const { expect } = chai;
@@ -17,15 +18,43 @@ before((done) => {
 describe('Comment model', () => {
   before((done) => {
     movieFactory()
-      .then((movie) => {
-        this.movie = movie;
-        done();
+      .then(movie => movie.id)
+      .then(movieId => commentFactory(movieId))
+      .then((comment) => {
+        this.comment = comment;
+        return done();
       });
   });
 
   describe('model attributes', () => {
     describe('body', () => {
-      it('should be of type TEXT');
+      it('should be of type TEXT', (done) => {
+        const fieldType = this.comment.rawAttributes.body.type.constructor.key;
+        expect(fieldType).to.be.equal('TEXT');
+        done();
+      });
+    });
+
+    describe('MovieId', () => {
+      it('should be of type INTEGER', (done) => {
+        const fieldType = this.comment.rawAttributes.MovieId.type.constructor.key;
+        expect(fieldType).to.be.equal('INTEGER');
+        done();
+      });
+    });
+  });
+
+  describe('model associations', () => {
+    it('should be associated with Movie', (done) => {
+      const { associations } = this.comment.constructor;
+      expect(associations).to.have.property('Movie');
+      done();
+    });
+
+    it('should belongs to Movie ', (done) => {
+      const { associationType } = this.comment.constructor.associations.Movie;
+      expect(associationType).to.be.equal('BelongsTo');
+      done();
     });
   });
 });
