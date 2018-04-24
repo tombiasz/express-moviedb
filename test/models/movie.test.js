c3b2162cebb64b37a5ca376c0add0b96339d3dd6
tombiasz <tomasz.m.nowacki@gmail.com>
2018-04-24 15:49:57 +0200

    test movie attributes and associations

--- test/models/movie.test.js
+++ test/models/movie.test.js
@@ -1,12 +1,52 @@
 const chai = require('chai');
 const mocha = require('mocha');
 
+const movieFactory = require('../factories/movie');
+
 
 const { expect } = chai;
 const { describe, it, before } = mocha;
 
 describe('Movie model', () => {
-  describe('title', () => {
-    it('should be of type String');
+  before((done) => {
+    movieFactory()
+      .then((movie) => {
+        this.movie = movie;
+        done();
+      });
+  });
+
+  describe('model attributes', () => {
+    describe('title', () => {
+      it('should be of type STRING', (done) => {
+        const { movie } = this;
+        const fieldType = movie.rawAttributes.title.type.constructor.key;
+        expect(fieldType).to.be.equal('STRING');
+        done();
+      });
+    });
+
+    describe('document', () => {
+      it('should be of type JSONB', (done) => {
+        const { movie } = this;
+        const fieldType = movie.rawAttributes.document.type.constructor.key;
+        expect(fieldType).to.be.equal('JSONB');
+        done();
+      });
+    });
+  });
+
+  describe('model associations', () => {
+    it('should be associated with Comments', (done) => {
+      const { associations } = this.movie.constructor;
+      expect(associations).to.have.property('Comments');
+      done();
+    });
+
+    it('should have many Comments ', (done) => {
+      const { associationType } = this.movie.constructor.associations.Comments;
+      expect(associationType).to.be.equal('HasMany');
+      done();
+    });
   });
 });
